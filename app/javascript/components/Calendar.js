dabe14154a6e0795790e155cfa37764255374028
深澤　寛太 <kantafkzw@gmail.com>
2022-02-13 20:43:48 +0900

    リファクタリング（主にファイル整理）

--- app/javascript/components/Calendar.js
+++ app/javascript/components/Calendar.js
@@ -1,24 +1,17 @@
-// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
-// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
-// of the page.
-
 import React from 'react';
-import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
 import axios from 'axios';
 import FullCalendar from '@fullcalendar/react' ;
 import dayGridPlugin from '@fullcalendar/daygrid' ;
 import interactionPlugin from '@fullcalendar/interaction';
 import allLocales from '@fullcalendar/core/locales-all';
 
-
-export default class CalendarApp extends React.Component {
-  dayCellContent = (e) => {
+function Calendar() {
+  const   dayCellContent = (e) => {
     (e.dayNumberText = e.dayNumberText.replace('日', ''))
   }
 
-  render() {
-    return (
+  return (
+    <>
       <div class= 'calendar'>
         <FullCalendar
           plugins={[ dayGridPlugin, interactionPlugin ]}
@@ -29,14 +22,13 @@ export default class CalendarApp extends React.Component {
           timeZone='Asia/Tokyo'
           firstDay={0}
           businessHours={true}
-          dayCellContent={this.dayCellContent}
+          dayCellContent={dayCellContent}
           eventColor='#63ceef'
           eventTextColor='#000000'
           events={'/events.json'}
-          select={this.handleDateSelect}
           />  
-      </div>    
-    )
-  }
+      </div> 
+    </>   
+  )
 }
-
+export default Calendar
