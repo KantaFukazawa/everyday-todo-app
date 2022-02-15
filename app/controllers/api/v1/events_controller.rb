10e93c7964164aa8163ee4c50a6bb7a23db8e980
深澤　寛太 <kantafkzw@gmail.com>
2022-02-15 16:14:14 +0900

    eventscontrollerをAPI化、routesを変更

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,4 +1,4 @@
-class EventsController < ApplicationController
+class Api::V1::EventsController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create, :update, :destroy]
 
   def index
@@ -25,20 +25,20 @@ class EventsController < ApplicationController
     end
   end
 
-  # def update
-  #   @event = current_user.events.find(params[:id])
-  #   if @event.update(event_params)
-  #     redirect_to root_path
-  #   else
-  #     flash.now[:error] = '更新できませんでした'
-  #   end
-  # end
+  def update
+    @event = current_user.events.find(params[:id])
+    if @event.update(event_params)
+      redirect_to root_path
+    else
+      flash.now[:error] = '更新できませんでした'
+    end
+  end
 
-  # def destroy
-  #   @event = current_user.events.find(params[:id])
-  #   event.destroy!
-  #   redirect_to root_path, notice: '削除しました'
-  # end
+  def destroy
+    @event = current_user.events.find(params[:id])
+    event.destroy!
+    redirect_to root_path, notice: '削除しました'
+  end
 
   private
   def event_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,18 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   authenticated :user do
-    root :to => 'events#index', :as => "user_authenticated_root"
-    resources :events, only:[:index, :show , :new, :create] 
+    root :to => 'calendars#index', :as => "user_authenticated_root"
   end
 
   resource :profile, only: [:show, :edit, :update]
+
+  namespace :api do
+    namespace :v1 do
+      resources :events, only:[:index, :show , :new, :create]
+    end
+  end
   
-  root 'top#index'
+  root 'calendars#index'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
